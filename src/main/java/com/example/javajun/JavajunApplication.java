039449d72060f1e061acbfe6f69e48126f705269
VasilevID1111 <VasilevID1111@mail.ru>
2023-02-05 21:31:00 +0500

    initial project
    initial project

---
+++ src/main/java/com/example/javajun/JavajunApplication.java
@@ -0,0 +1,13 @@
+package com.example.javajun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavajunApplication.class, args);
+	}
+
+}
